ebb1a251a4e1533c368ad06335c55a96bb7e4bdc
vickler charles <charlesvikler@gmail.com>
2019-07-31 10:30:26 -0500

    añadi avatar uploader

---
+++ src/ts/components/AvatarUploader/AvatarUploader.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react'
+import Avatar from 'react-avatar-edit'
+
+interface State {
+  preview: any;
+  src: any;
+}
+
+class AvatarUploader extends React.Component<{}, State> {
+  constructor(props: any) {
+    super(props)
+    const src = './example/einshtein.jpg'
+    this.state = {
+      preview: null,
+      src
+    }
+    this.onCrop = this.onCrop.bind(this)
+    this.onClose = this.onClose.bind(this)
+    this.onBeforeFileLoad = this.onBeforeFileLoad.bind(this)
+  }
+
+  onClose() {
+    this.setState({preview: null})
+  }
+
+  onCrop(preview: any) {
+    this.setState({preview})
+  }
+
+  onBeforeFileLoad(elem: any) {
+    if(elem.target.files[0].size > 71680){
+      alert("File is too big!");
+      elem.target.value = "";
+    };
+  }
+
+  render () {
+    return (
+      <div>
+        <Avatar
+          width={390}
+          height={295}
+          onCrop={this.onCrop}
+          onClose={this.onClose}
+          onBeforeFileLoad={this.onBeforeFileLoad}
+          src={this.state.src}
+        />
+        <img src={this.state.preview} alt="Preview" />
+      </div>
+    )
+  }
+}
+
+export default AvatarUploader;
